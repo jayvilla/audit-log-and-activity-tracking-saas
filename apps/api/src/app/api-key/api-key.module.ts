b0590c14cd56e0126865a52cccf68bfd9c6e6fd4
Jeff Villarina <villarina.jeffrey@gmail.com>
2025-12-30 07:59:42 -0800

    fix api-key module

--- apps/api/src/app/api-key/api-key.module.ts
+++ apps/api/src/app/api-key/api-key.module.ts
@@ -5,9 +5,10 @@ import { ApiKeyGuard } from './api-key.guard';
 import { ApiKeyController } from './api-key.controller';
 import { RateLimiterService } from './rate-limiter.service';
 import { ApiKeyEntity } from '../../entities/api-key.entity';
+import { AuthModule } from '../auth/auth.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([ApiKeyEntity])],
+  imports: [TypeOrmModule.forFeature([ApiKeyEntity]), AuthModule],
   controllers: [ApiKeyController],
   providers: [ApiKeyService, ApiKeyGuard, RateLimiterService],
   exports: [ApiKeyService, ApiKeyGuard, RateLimiterService],
